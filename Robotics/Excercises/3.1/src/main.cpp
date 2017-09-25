aa88a3afb0ec3ef5ec4d560cc86e04f90d923761
crowdedlight <crowdedhd@gmail.com>
2017-09-25 22:52:37 +0200

    did 3_1 and 3_2 but special case of r31 = 1

--- Robotics/Excercises/3.1/src/main.cpp
+++ Robotics/Excercises/3.1/src/main.cpp
@@ -10,13 +10,51 @@ using namespace std;
 using namespace rw::math;
 
 Rotation3D<> matrixRotate(char axis, float angle);
+Rotation3D<> matrixRotate(float rotationX, float rotationY, float rotationZ);
+RPY<> RotToRPY(Rotation3D<> &matrix);
 
 int main() {
 
     Rotation3D<> matrix = matrixRotate('x', 60);
     cout << matrix << endl << endl;
 
+    Rotation3D<> matrix2 = matrixRotate(60,30,40);
+    cout << matrix2 << endl << endl;
+
+    RPY<> matrixRPY = RotToRPY(matrix2);
+    cout << matrixRPY << endl << endl;
+
     return 0;
+}
+
+RPY<> RotToRPY(Rotation3D<> &matrix)
+{
+    double thetaY = asin(-matrix(2,0));
+    double thetaX, thetaZ;
+
+    //if r_31 != 1
+    if(matrix(2,0) != 1)
+    {
+        thetaX = atan2(cos(matrix(2,1)), cos(matrix(2,2)));
+        thetaZ = atan2(cos(matrix(1,0)), cos(matrix(0,0)));
+    } else
+    {
+        throw("R_31 = 1 error");
+    }
+
+    return RPY<>(thetaX, thetaY, thetaZ);
+}
+
+Rotation3D<> matrixRotate(float rotationX, float rotationY, float rotationZ)
+{
+    Rotation3D<> rotX = matrixRotate('x', rotationX);
+    Rotation3D<> rotY = matrixRotate('y', rotationY);
+    Rotation3D<> rotZ = matrixRotate('z', rotationZ);
+
+    Rotation3D<> rotXY = rotX.multiply(rotX, rotY);
+    Rotation3D<> rotXZ = rotXY.multiply(rotXY, rotZ);
+
+    return rotXZ;
 }
 
 Rotation3D<> matrixRotate(char axis, float angle)
